fa280eccebce88fedebb33625bc41fefc8ef3c85
cypher682 <78412060+cypher682@users.noreply.github.com>
2026-04-13 09:06:31 +0100

    feat: GitHub Actions CI/CD pipeline and S3 remote state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,9 +7,15 @@ terraform {
       version = "~> 5.0"
     }
   }
+    backend "s3" {
+    bucket  = "cloudguard-lite-tfstate-758620460011"
+    key     = "cloudguard-lite/terraform.tfstate"
+    region  = "us-east-1"
+  }
 }
+
+
 
 provider "aws" {
   region  = var.aws_region
-  profile = "cloudguard"
 }
